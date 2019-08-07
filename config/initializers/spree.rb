7942ba00f999b5e0de36ee786af32ea0db8f39c0
Oswaldo Zavala <oszavala@gmail.com>
2019-08-07 09:44:32 -0500

    Created Products

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -33,7 +33,7 @@ Spree.config do |config|
   # Frontend:
 
   # Custom logo for the frontend
-  # config.logo = "logo/solidus.svg"
+   config.logo = "logo/logo4-bake&cake-300x150.png"
 
   # Template to use when rendering layout
   # config.layout = "spree/layouts/spree_application"
@@ -42,7 +42,7 @@ Spree.config do |config|
   # Admin:
 
   # Custom logo for the admin
-  # config.admin_interface_logo = "logo/solidus.svg"
+   config.admin_interface_logo = "logo/logo3-bake&cake-300x45.png"
 
   # Gateway credentials can be configured statically here and referenced from
   # the admin. They can also be fully configured from the admin.
